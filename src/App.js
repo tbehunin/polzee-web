f89cc1cdd0bc26396419610e7c205eab72b56ae3
todd.behunin@nike.com <todd.behunin@nike.com>
2019-04-03 15:44:05 -0700

    wire up auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,5 +25,24 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+const signUpConfig = {
+  hideAllDefaults: true,
+  hiddenDefaults: ['username','phone_number'],
+  signUpFields: [{
+    label:"Email",
+    key:"username",
+    required: true,
+    displayOrder: 1,
+    type: 'string',
+    custom: false
+  },        
+  {
+    label:"Password",
+    key:"password",
+    required: true,
+    displayOrder: 2,
+    type: 'password',
+    custom: false
+  }]
+};
+export default withAuthenticator(App, { signUpConfig });
