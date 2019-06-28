5faaebeaa73c599e6c6d2464f2a705894e6fe7fe
todd.behunin@nike.com <todd.behunin@nike.com>
2019-06-28 15:58:00 -0700

    configure amplify auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from './config';
+
+Amplify.configure({
+    Auth: {
+        identityPoolId: config.get('AMPLIFYAUTH_IDENTITYPOOLID'),
+        region: config.get('AMPLIFYAUTH_REGION'),
+        userPoolId: config.get('AMPLIFYAUTH_USERPOOLID'),
+        userPoolWebClientId: config.get('AMPLIFYAUTH_USERPOOLCLIENTID'),
+        mandatorySignIn: false,
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
